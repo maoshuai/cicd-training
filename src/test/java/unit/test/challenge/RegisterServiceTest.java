1337ac4287a24033e6bcf6f9bf74eeb39952a2a0
maoshuai <zhucemid@gmail.com>
2020-07-05 22:31:00 +0800

    should_not_send_sms_given_user_without_phone

---
+++ src/test/java/unit/test/challenge/RegisterServiceTest.java
@@ -0,0 +1,27 @@
+package unit.test.challenge;
+
+import org.junit.jupiter.api.Test;
+import unit.test.challenge.support.SmsService;
+import unit.test.challenge.support.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RegisterServiceTest {
+
+    @Test
+    public void should_not_send_sms_given_user_without_phone(){
+        SmsService smsService = mock(SmsService.class);
+
+        User user = new User();
+        user.setPhone(null);
+
+        RegisterService registerService = new RegisterService(smsService);
+        registerService.register(user);
+
+        verify(smsService, never()).send(user);
+    }
+
+
+
+}
