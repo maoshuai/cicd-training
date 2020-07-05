f2749f75412cb2b660dea538ba95b5e63e5e114e
maoshuai <zhucemid@gmail.com>
2020-07-05 22:27:49 +0800

    should_return_discount_given_age_older_than_60

--- src/test/java/unit/test/challenge/UserServiceTest.java
+++ src/test/java/unit/test/challenge/UserServiceTest.java
@@ -19,6 +19,12 @@ class UserServiceTest {
         assertEquals(DISCOUNT,discount);
     }
 
+    @Test
+    public void should_return_discount_given_age_older_than_60(){
+        User user = getUserOfAge(61);
+        double discount = userService.getDiscount(user);
+        assertEquals(DISCOUNT,discount);
+    }
 
 
     private User getUserOfAge(int i) {
