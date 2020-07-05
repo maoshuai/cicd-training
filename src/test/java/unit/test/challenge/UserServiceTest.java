428a9b769ac1ca8ffd17855b236cfaa24b1d070b
maoshuai <zhucemid@gmail.com>
2020-07-05 22:26:32 +0800

    should_return_discount_given_age_of_60

---
+++ src/test/java/unit/test/challenge/UserServiceTest.java
@@ -0,0 +1,29 @@
+package unit.test.challenge;
+
+import org.junit.jupiter.api.Test;
+import unit.test.challenge.support.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    private UserService userService = new UserService();
+
+    private static double DISCOUNT = 0.5;
+    private static double NO_DISCOUNT=1;
+
+    @Test
+    public void should_return_discount_given_age_of_60(){
+        User user = getUserOfAge(60);
+        double discount = userService.getDiscount(user);
+
+        assertEquals(DISCOUNT,discount);
+    }
+
+
+
+    private User getUserOfAge(int i) {
+        User user = new User();
+        user.setAge(i);
+        return user;
+    }
+}
