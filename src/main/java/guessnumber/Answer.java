d794b26ccffbaa15fdb0088f3f4799db52696067
maoshuai <zhucemid@gmail.com>
2020-07-07 14:23:24 +0800

    refactor: introduce constant

--- src/main/java/guessnumber/Answer.java
+++ src/main/java/guessnumber/Answer.java
@@ -7,6 +7,8 @@ import java.util.Set;
 
 public class Answer {
     private static final String INCORRECT_INPUT_MSG = "输⼊不正确，重新输⼊";
+    public static final int ANSWER_DIGITS_LEN = 4;
+
     private List<String> answerDigits;
     public Answer(List<String> answerDigits){
         this.answerDigits = answerDigits;
@@ -21,7 +23,7 @@ public class Answer {
 
         String[] inputDigitsArr = inputDigits.split(" ");
 
-        if (inputDigitsArr.length != 4) {
+        if (inputDigitsArr.length != ANSWER_DIGITS_LEN) {
             return INCORRECT_INPUT_MSG;
         }
 
@@ -34,7 +36,7 @@ public class Answer {
         int onlyDigitRight =0;
 
 
-        for (int i=0;i<4;i++){
+        for (int i = 0; i< ANSWER_DIGITS_LEN; i++){
             if(inputDigitsArr[i].equals(answerDigits.get(i))){
                 exactlyRight ++;
             }else if (answerDigits.contains(inputDigitsArr[i])){
