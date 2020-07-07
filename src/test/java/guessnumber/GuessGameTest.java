3a381cb5bb67828f31c29d7dcf7de8f25c5a90b6
maoshuai <zhucemid@gmail.com>
2020-07-07 15:41:24 +0800

    should_return_false_when_missed_max_trying_times

--- src/test/java/guessnumber/GuessGameTest.java
+++ src/test/java/guessnumber/GuessGameTest.java
@@ -25,6 +25,25 @@ class GuessGameTest {
     }
 
     @Test
+    void should_return_false_when_missed_max_trying_times() {
+
+        // always try wrong input, there should be at most 6 times
+        Answer answer = mock(Answer.class);
+        when(answer.guess("2 2 3 4")).thenReturn("3A1B");
+
+        GameInputService gameInputService = mock(GameInputService.class);
+        when(gameInputService.readLine()).thenReturn("2 2 3 4");
+
+        GuessGame guessGame = new GuessGame();
+        guessGame.setAnswer(answer);
+        guessGame.setGameInputService(gameInputService);
+
+
+        assertFalse(guessGame.play());
+
+    }
+
+    @Test
     void should_try_once_given_right_input() {
 
         // try a right input
