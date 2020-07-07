89dec8e791bd972ba039f81c70dc55cd3ad96dc0
maoshuai <zhucemid@gmail.com>
2020-07-07 09:13:27 +0800

    should_generate_digits_not_repeated

--- src/test/java/guessnumber/AnswerGeneratorTest.java
+++ src/test/java/guessnumber/AnswerGeneratorTest.java
@@ -2,17 +2,27 @@ package guessnumber;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AnswerGeneratorTest {
 
     @Test
-    void should_generate_four_digits_with_space() {
-        AnswerGenerator generator = new AnswerGenerator();
-        String answer = generator.generate();
+    void should_generate_four_digits_with_spaces() {
+        String answer = new AnswerGenerator().generate();
         Pattern compile = Pattern.compile("[0-9] [0-9] [0-9] [0-9]");
         assertTrue(compile.matcher(answer).matches());
+    }
+
+    @Test
+    void should_generate_digits_not_repeated() {
+        String answer = new AnswerGenerator().generate();
+        String [] digits = answer.split(" ");
+        Set digitsSet = new HashSet(Arrays.asList(digits));
+        assertEquals(digitsSet.size(), digits.length);
     }
 }
