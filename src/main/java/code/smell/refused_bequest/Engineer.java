5afac452704540cebe18f8664d64284988a5c4fa
maoshuai <zhucemid@gmail.com>
2020-07-12 22:35:48 +0800

    solve refuse bequest

--- src/main/java/code/smell/refused_bequest/Engineer.java
+++ src/main/java/code/smell/refused_bequest/Engineer.java
@@ -3,6 +3,6 @@ package code.smell.refused_bequest;
 public class Engineer extends Employee {
     @Override
     public boolean hasSalesQuota(int quota) {
-        throw new UnsupportedOperationException("Engineer has no sales quota");
+        return false;
     }
 }
