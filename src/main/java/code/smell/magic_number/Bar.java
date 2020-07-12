e29c3152a920b14e51da9a5b1e06230938723cb2
maoshuai <zhucemid@gmail.com>
2020-07-12 22:16:24 +0800

    resolve magic number

--- src/main/java/code/smell/magic_number/Bar.java
+++ src/main/java/code/smell/magic_number/Bar.java
@@ -2,12 +2,11 @@ package code.smell.magic_number;
 
 public class Bar {
 
+    public static final int AGE_FOR_DRINK = 21;
+    public static final int STATUS_EXEMPT = 1;
+
     public boolean doSomething(int age, int status) {
         //Question: what does 21 and 1 means?
-        if (age > 21 && status == 1) {
-            return true;
-        } else {
-            return false;
-        }
+        return age > AGE_FOR_DRINK && status == STATUS_EXEMPT;
     }
 }
