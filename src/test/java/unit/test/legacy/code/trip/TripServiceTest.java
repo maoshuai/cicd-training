2bbaf0811bcca48f333ce7406782ac7f89b2dec1
maoshuai <zhucemid@gmail.com>
2020-07-13 09:59:16 +0800

    add test case: should_return_empty_trip_list_given_logged_in_user_not_a_friend_when_get_trips_of_user

--- src/test/java/unit/test/legacy/code/trip/TripServiceTest.java
+++ src/test/java/unit/test/legacy/code/trip/TripServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import unit.test.legacy.code.exceptions.UserNotLoggedInException;
 import unit.test.legacy.code.user.User;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TripServiceTest {
@@ -17,5 +19,20 @@ class TripServiceTest {
         };
 
         assertThrows(UserNotLoggedInException.class, ()-> notLoggedInTripService.getTripsByUser(new User()));
+    }
+
+    @Test
+    void should_return_empty_trip_list_given_logged_in_user_not_a_friend_when_get_trips_of_user() throws UserNotLoggedInException {
+        User nonFriendUser = new User();
+        User queryUser = new User();
+        TripService tripService = new TripService(){
+            @Override
+            protected User getLoggedInUser() {
+                return nonFriendUser;
+            }
+        };
+
+        List<Trip> tripList = tripService.getTripsByUser(queryUser);
+        assertEquals(0, tripList.size());
     }
 }
